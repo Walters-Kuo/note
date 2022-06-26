f6d2eb785d1ff279ca1d2b3bf8ec459aa281a9d2
Walters <walters.kuo66@gmail.com>
2022-06-26 10:34:06 +0800

    first commit

---
+++ command.sh
@@ -0,0 +1,14 @@
+#bash
+
+cd - #change the current directory to the previous working directory
+cd ~ #return to user's home directory
+
+!! on command line will be replaced by last command issued:
+mv /etc/hostname /etc/hostname.old
+mv: rename /etc/hostname to /etc/hostname.old: Operation not permitted
+sudo !! #sudo mv /etc/hostname /etc/hostname.old
+
+!$ on command line will be replaced by last command arguments:
+mkdir /tmp/test
+cd !$ #cd /tmp/test
+
