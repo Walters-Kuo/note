4c35f02e1d05ee895ab0d089c3ec1ca19f77b116
Walters <walters.kuo66@gmail.com>
2022-07-28 15:47:23 +0800

    daily update

--- command.sh
+++ command.sh
@@ -12,6 +12,12 @@ sudo !! #sudo mv /etc/hostname /etc/hostname.old
 mkdir /tmp/test
 cd !$ #cd /tmp/test
 
+sudo user vs su user or sudo su vs su - 
+su requires the password of the target account, while sudo requires the password of the current user. 
+
+#hardware
+cat /proc/cpuinfo | grep 'processor' | wc -l
+
 #hotkey
 ctr + L to clear the screen
 
@@ -42,3 +48,23 @@ brew info python
 #network
 lsof -nP -iTCP -sTCP:LISTEN
 lsof -n -i | grep LISTEN
+
+        Format/result           |       Command              |          Output
+--------------------------------+----------------------------+------------------------------
+YYYY-MM-DD                      | date -I                    | $(date -I)
+YYYY-MM-DD_hh:mm:ss             | date +%F_%T                | $(date +%F_%T)
+YYYYMMDD_hhmmss                 | date +%Y%m%d_%H%M%S        | $(date +%Y%m%d_%H%M%S)
+YYYYMMDD_hhmmss (UTC version)   | date --utc +%Y%m%d_%H%M%SZ | $(date --utc +%Y%m%d_%H%M%SZ)
+YYYYMMDD_hhmmss (with local TZ) | date +%Y%m%d_%H%M%S%Z      | $(date +%Y%m%d_%H%M%S%Z)
+YYYYMMSShhmmss                  | date +%Y%m%d%H%M%S         | $(date +%Y%m%d%H%M%S)
+YYYYMMSShhmmssnnnnnnnnn         | date +%Y%m%d%H%M%S%N       | $(date +%Y%m%d%H%M%S%N)
+YYMMDD_hhmmss                   | date +%y%m%d_%H%M%S        | $(date +%y%m%d_%H%M%S)
+Seconds since UNIX epoch:       | date +%s                   | $(date +%s)
+Nanoseconds only:               | date +%N                   | $(date +%N)
+Nanoseconds since UNIX epoch:   | date +%s%N                 | $(date +%s%N)
+ISO8601 UTC timestamp           | date --utc +%FT%TZ         | $(date --utc +%FT%TZ)
+ISO8601 UTC timestamp + ms      | date --utc +%FT%T.%3NZ     | $(date --utc +%FT%T.%3NZ)
+ISO8601 Local TZ timestamp      | date +%FT%T%Z              | $(date +%FT%T%Z)
+YYYY-MM-DD (Short day)          | date +%F\(%a\)             | $(date +%F\(%a\))
+YYYY-MM-DD (Long day)           | date +%F\(%A\)             | $(date +%F\(%A\))
+
